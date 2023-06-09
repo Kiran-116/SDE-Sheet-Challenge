0211d7d613c858e6060994e0378b15a19c967f11
Kiran <gkirankumar503@gmail.com>
2023-06-09 18:13:07 +0530

    Pascal Triangle Updated

--- Easy/Arrays/Pascals_Triangle.cpp
+++ Easy/Arrays/Pascals_Triangle.cpp
@@ -22,3 +22,39 @@ vector<vector<long long int>> printPascal(int n)
 
     return ans;
 }
+
+
+// 1st Type -> find nth row and cth column
+int nCr(int n, int r) {
+    long long int res = 1;
+    for (int i = 0 ; i < r ; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+
+
+// Optimal Code Studio
+// Type - 2
+vector<long long int> generateRow(int row) {
+    long long ans = 1;
+    vector<long long int> ansRow;
+    ansRow.push_back(1);
+    for (int col = 1 ; col < row ; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+
+// Type - 3
+vector<vector<long long int>> printPascal(int n) 
+{
+    vector<vector<long long int>> ans;
+    for (int i = 1 ; i <= n ; i++) {
+        ans.push_back(generateRow(i));
+    }
+    return ans;
+}
