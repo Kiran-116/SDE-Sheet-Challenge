2acafd672a246a334374c122a51ce6195e9f9db7
Kiran <gkirankumar503@gmail.com>
2023-07-05 17:32:26 +0530

    N Queens -> solved

---
+++ Hard/Recursion and Backtracking/N_Queens.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void addsolution(vector<vector<int>> &ans, vector<vector<int>> &board, int n)
+{
+    vector<int> temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            temp.push_back(board[i][j]);
+        }
+    }
+    ans.push_back(temp);
+}
+bool issafe(int row, int col, vector<vector<int>> &board, int n)
+{
+    int x = row;
+    int y = col;
+    while (y >= 0)
+    {
+        if (board[x][y] == 1)
+            return false;
+        y--;
+    }
+    x = row;
+    y = col;
+    while (x >= 0 && y >= 0)
+    {
+        if (board[x][y] == 1)
+            return false;
+        y--;
+        x--;
+    }
+    x = row;
+    y = col;
+    while (x < n && y >= 0)
+    {
+        if (board[x][y] == 1)
+            return false;
+        y--;
+        x++;
+    }
+}
+void solve(int col, vector<vector<int>> &ans, vector<vector<int>> &board, int n)
+{
+    if (col == n)
+    {
+        addsolution(ans, board, n);
+        return;
+    }
+    for (int row = 0; row < n; row++)
+    {
+        if (issafe(row, col, board, n))
+        {
+            board[row][col] = 1;
+            solve(col + 1, ans, board, n);
+            board[row][col] = 0;
+        }
+    }
+}
+
+vector<vector<int>> solveNQueens(int n)
+{
+    vector<vector<int>> board(n, vector<int>(n, 0));
+    vector<vector<int>> ans;
+    solve(0, ans, board, n);
+    return ans;
+}
+// Time -> O(N!)
+// Space -> O(N^2)
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+  public:
+    void solve(int col, vector < string > & board, vector < vector < string >> & ans, vector < int > & leftrow, vector < int > & upperDiagonal, vector < int > & lowerDiagonal, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+        if (leftrow[row] == 0 && lowerDiagonal[row + col] == 0 && upperDiagonal[n - 1 + col - row] == 0) {
+          board[row][col] = 'Q';
+          leftrow[row] = 1;
+          lowerDiagonal[row + col] = 1;
+          upperDiagonal[n - 1 + col - row] = 1;
+          solve(col + 1, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
+          board[row][col] = '.';
+          leftrow[row] = 0;
+          lowerDiagonal[row + col] = 0;
+          upperDiagonal[n - 1 + col - row] = 0;
+        }
+      }
+    }
+
+  public:
+    vector < vector < string >> solveNQueens(int n) {
+      vector < vector < string >> ans;
+      vector < string > board(n);
+      string s(n, '.');
+      for (int i = 0; i < n; i++) {
+        board[i] = s;
+      }
+      vector < int > leftrow(n, 0), upperDiagonal(2 * n - 1, 0), lowerDiagonal(2 * n - 1, 0);
+      solve(0, board, ans, leftrow, upperDiagonal, lowerDiagonal, n);
+      return ans;
+    }
+};
+// 
