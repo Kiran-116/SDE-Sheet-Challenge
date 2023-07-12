82987438ce836935c0ab3e6f8e6b0caba98ed06e
Kiran <gkirankumar503@gmail.com>
2023-07-12 08:55:04 +0530

    Leetcode Sol add in Single Element in sorted array

--- Easy/Binary Search/Single_Ele_SortedArray.cpp
+++ Easy/Binary Search/Single_Ele_SortedArray.cpp
@@ -44,12 +44,6 @@ int BS_SingleElement(int low, int high, vector<int> &arr, int n) {
 	if (arr[mid] != arr[mid-1] && arr[mid] != arr[mid+1]) {
 		return arr[mid];
 	}
-	// if (arr[mid] != arr[mid-1] && mid == n-1) {
-	// 	return arr[mid];
-	// }
-	// if (arr[mid] != arr[mid+1] && mid == 0) {
-	// 	return arr[mid];
-	// }
 	else if (arr[mid]==arr[mid-1] && mid%2 != 0 || arr[mid]==arr[mid+1] && mid%2 == 0) { 		// odd index
 		BS_SingleElement(mid + 1, high, arr, n);
 	}
@@ -66,3 +60,29 @@ int singleNonDuplicate(vector<int>& arr)
 
 // Time: O(log n)
 // Space: O(1) 	
+
+
+// Leetcode:
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+      int n=nums.size();
+        if(n==1)return nums[0];
+        if(nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int st=1,end=n-2;
+        while(st<=end){
+            int mid=(st+end)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
+
+            if((mid%2==1 && nums[mid]==nums[mid-1]) || (mid%2==0 && nums[mid]==nums[mid+1]) ){
+                st=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+};
+// Time: O(log n)
+// Space: O(1)
