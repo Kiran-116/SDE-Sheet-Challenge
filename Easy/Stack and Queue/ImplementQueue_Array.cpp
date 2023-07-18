aca8f5505db41a5d6a522dd40a8b75e12bf27377
Kiran <gkirankumar503@gmail.com>
2023-07-18 13:51:25 +0530

    Implement Queue using Arrays -> modified

--- Easy/Stack and Queue/ImplementQueue_Array.cpp
+++ Easy/Stack and Queue/ImplementQueue_Array.cpp
@@ -39,3 +39,58 @@ public:
 		return  ans;
 	}
 };
+
+// Coding Ninjas:
+class Queue {
+    int *arr;
+    int qfront;
+    int qrear;
+    int size;
+public:
+    Queue() {
+        size = 100001;
+        qfront = 0;
+        qrear = 0;
+        arr = new int[size];
+    }
+
+    /*----------------- Public Functions of Queue -----------------*/
+
+    bool isEmpty() {
+        if (qfront == qrear) {
+            return true;
+        }
+        return false;
+    }
+
+    void enqueue(int data) {
+        if (qrear == size) {
+            cout << "Queue is Full" << endl;
+        }
+        else {
+            arr[qrear] = data;
+            qrear++;
+        }
+    }
+
+    int dequeue() {
+        if (isEmpty()) {
+            return -1;
+        }
+        int ans = arr[qfront];
+        arr[qfront] = -1;
+        qfront++;
+        if (qfront == qrear) {
+            qfront = 0;
+            qrear = 0;
+        }
+        return ans;
+    }
+
+    int front() {
+        if (qfront == qrear) {
+            return -1;
+        }
+        return arr[qfront];
+    }
+};
