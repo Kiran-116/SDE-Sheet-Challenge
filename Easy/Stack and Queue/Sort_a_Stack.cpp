88031a95b63accecae682fad2774ada44235d0f8
Kiran <gkirankumar503@gmail.com>
2023-07-20 18:02:16 +0530

    Sort a Stack -> solved

---
+++ Easy/Stack and Queue/Sort_a_Stack.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortedInsert(stack<int> &stack, int num) {
+    // base case
+    if(stack.empty() || (!stack.empty() && stack.top() < num)) {
+        stack.push(num);
+        return;
+    }
+
+    // if top is > num, keep it aside.
+    int n = stack.top();
+    stack.pop();
+
+    // recursive call
+    sortedInsert(stack, num);
+
+    stack.push(n);
+}
+
+void sortStack(stack<int> &stack)
+{
+	// base case
+    if (stack.empty()) {
+        return;
+    }
+
+    int num = stack.top();
+    stack.pop();
+
+    // recursive call
+    sortStack(stack);
+
+    sortedInsert(stack, num);
+}
+
+// Time -> 
